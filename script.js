d51c9c4de3540f13dd44204b38f6487603a3c766
Lydoww <alexis94340@gmail.com>
2025-02-20 16:21:12 +0100

    responsive tablet and mobile

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const URL = "https://fakestoreapi.com/products";
 
+const divButtonElem = document.getElementById('sortButton')
+const sortButton = document.createElement('button')
+sortButton.textContent = 'sort by price'
+divButtonElem.appendChild(sortButton)
+
+
 async function getProducts() {
   const response = await fetch(URL);
   const data = await response.json();
@@ -17,16 +23,29 @@ async function showProducts() {
 
 function createCard(product) {
   const productCard = document.createElement("div");
-productCard.classList.add('product-card')
+  productCard.classList.add("product-card");
+
   const titleElem = document.createElement("h2");
   titleElem.innerHTML = product.title;
+
+  const priceElem = document.createElement("p");
+  priceElem.innerHTML = `${product.price} $`;
+
   const imageElem = document.createElement("img");
   imageElem.src = product.image;
+
   const infoElem = document.createElement("p");
   infoElem.innerHTML = product.description;
-  productCard.append(titleElem, imageElem, infoElem);
+  
+  productCard.append(titleElem, imageElem, infoElem, priceElem);
 
   document.querySelector(".products").appendChild(productCard);
 }
+
+function sortPrice() {
+
+}
+
+sortButton.addEventListener('click', sortPrice())
 
 showProducts();
